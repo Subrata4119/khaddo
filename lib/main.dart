6b34ef3853facc9df8181836bbc0a63ae5e5dc14
Subrata4119 <Subrata4119@users.noreply.github.com>
2023-08-27 11:52:19 +0600

    project setUp

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "khaddo App",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+
+    );
+  }
+}
